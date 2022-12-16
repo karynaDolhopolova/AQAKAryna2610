e19c4ddd09f3fed2b14ab861a65b4533c4b216ec
karynadolhopolova <karyna.dol17@gmail.com>
2022-12-16 20:53:02 +0200

    collections

--- src/main/java/homeWork89/Lesson89.java
+++ src/main/java/homeWork89/Lesson89.java
@@ -1,2 +1,19 @@
-package homeWork89;public class Lesson89 {
+package homeWork89;
+
+import java.util.*;
+
+public class Lesson89 {
+    public static void main(String[] args) {
+        String products = "butter, butter, sausage, milk";
+        correction(products);
+
+    }
+
+    private static void correction(String str) {
+        List<String> productsList = new ArrayList<>(Arrays.asList(str.replace(" ", "").split(",")));
+        Set<String> listOfProducts = new HashSet<>(productsList);
+        for (String p : listOfProducts) {
+            System.out.println(p.toUpperCase().charAt(0) + p.substring(1));
+        }
+    }
 }
